898862a32f51894fa2e538ddd98ee5b2cb5043b7
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2021-02-24 17:39:59 +0900

    Update NNutils.py

--- DNN-analysis/NNutils.py
+++ DNN-analysis/NNutils.py
@@ -5,6 +5,50 @@ from sklearn.metrics import classification_report, accuracy_score
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import RandomForestRegressor as RFR
+
+def RandomForestRegressor(X, y, state=2000):
+    train_data_bs, test_data_bs, train_labels_bs, test_labels_bs = train_test_split(X, y, test_size=0.2)
+    rg = RFR(n_jobs=-1, random_state=state)
+    rg.fit(train_data_bs,train_labels_bs)
+    predicted_labels_bs = rg.predict(test_data_bs)
+    print('accuracy socore', rg.score(test_data_bs, test_labels_bs))
+    
+    
+def create_new_column(df, new, name1, name2):
+    df[new]=df[name1]-df[name2]
+    return df
+
+def normarize(X, MinMax=True):
+    if MinMax:
+        Xs = MinMaxScaler().fit_transform(X)
+    else:
+        Xs = preprocessing.StandardScaler().fit_transform(X)
+    return Xs
+
+
+def create_target(df, num_cls=10):
+    vst=[]
+    for cls in range(num_cls):
+        if cls ==0 or cls==1 or cls==2:
+            num = df[df.column == cls].shape[0]
+            for _ in range(num):
+                vst.append(np.array(df1.loc['L0']))
+        elif cls ==3 or cls==4 or cls==5:
+            num = df[df.column == cls].shape[0]
+            for _ in range(num):
+                vst.append(np.array(df1.loc['L1']))
+        elif cls ==6 or cls==6 or cls==7:
+            num = df[df.column == cls].shape[0]
+            for _ in range(num):
+                vst.append(np.array(df1.loc['L2']))
+        elif cls >7:
+            num = df[df.column == cls].shape[0]
+            for _ in range(num):
+                vst.append(np.array(df1.loc['l3']))
+    return pd.DataFrame(np.vstack(vst))
+    
 
 
 
